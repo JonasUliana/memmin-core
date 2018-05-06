608fe8851c08cd7b3acec6bf613c343d89419cf9
Jonas (0x000027) Uliana <dev.uliana@gmail.com>
2018-05-06 03:49:13 +0000

    adicionado exemplo simples de leitura de um ponteiro do tipo Int64

---
+++ Uiana.Library.Memmim/Uiana.Library.Memmim.Sample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace Uiana.Library.Memmim.Sample {
+    class Program {
+        static void Main() {
+            var processName = "DigOrDie";
+            var address = 0x15_2A_72_C4;
+
+            var memmim = new Memmim();
+            memmim.SetProcessPidByName(processName);
+
+            var readed = memmim.PrimitiveRead64Bits((IntPtr)address, 12);
+
+            WriteLine($"Lidos {readed.BytesRead} byte(s) do total de {readed.Buffer.Length} parametrizados");
+
+            // Caso o sistema for little-endian, o array de bytes precisa
+            // ser invertido.
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(readed.Buffer);
+
+            WriteLine($"Conteúdo: {BitConverter.ToInt64(readed.Buffer, 0)}");
+#if DEBUG
+            WriteLine("Pressione uma tecla para sair...");
+            ReadKey();
+#endif
+        }
+    }
+}
