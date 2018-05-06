8b8e04c005a93d6adf8802644425d5e221fbd8b5
Jonas (0x000027) Uliana <dev.uliana@gmail.com>
2018-05-06 03:08:06 +0000

    adicionada função para enumeração de processos

---
+++ Uiana.Library.Memmim/Uiana.Library.Memmim/Memmim.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using static System.Diagnostics.Process;
+using System.Threading.Tasks;
+
+namespace Uiana.Library.Memmim {
+    /// <summary>
+    /// Permite o gerenciamento remoto de outro processo.
+    /// </summary>
+    public class Memmim {
+        #region Propriedades
+
+        /// <summary>
+        /// Instância de processo remota atualmente consumida.
+        /// </summary>
+        public Process Process { get; private set; }
+
+        #endregion
+
+        /// <summary>
+        /// Parametriza <see cref="Process"/> com a assinatura de nome
+        /// especificada na função.
+        /// </summary>
+        /// <param name="signature">No do processo que irá ser indexado.</param>
+        public void SetProcessPidByName(string signature)
+        {
+            Process = GetProcessesByName(signature ?? throw new ArgumentException(nameof(signature)))
+                .Where(p => p.ProcessName == signature)
+                .Select(p => p)
+                .FirstOrDefault();
+        }
+    }
+}
