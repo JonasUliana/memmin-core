97b87e221a073c428489594be6f99fff5216ddf5
Jonas (0x000027) Uliana <dev.uliana@gmail.com>
2018-05-06 03:51:54 +0000

    adicionado função genérica de escrita na memória WriteProcessMemory

--- Uiana.Library.Memmim/Uiana.Library.Memmim/Libraries/Kernel32.cs
+++ Uiana.Library.Memmim/Uiana.Library.Memmim/Libraries/Kernel32.cs
@@ -36,5 +36,18 @@ namespace Uiana.Library.Memmim.Libraries {
         [DllImport("kernel32.dll")]
         public static extern bool ReadProcessMemory(int hProcess,
             IntPtr lpBaseAddress, byte[] lpBuffer, int dwSize, ref ulong lpNumberOfBytesRead);
+
+        /// <summary>
+        /// Usado para gravar dados em um processo remoto.
+        /// </summary>
+        /// <param name="hProcess">Um identificador para a memória do processo a ser modificada.</param>
+        /// <param name="lpBaseAddress">Um ponteiro para o endereço base no processo especificado no qual os dados são gravados.</param>
+        /// <param name="lpBuffer">Um ponteiro para o buffer que contém dados a serem gravados no espaço de endereço do processo especificado.</param>
+        /// <param name="dwSize">O número de bytes a serem gravados no processo especificado.</param>
+        /// <param name="lpNumberOfBytesWritten">Um ponteiro para uma variável que recebe o número de bytes transferidos para o processo especificado.</param>
+        /// <returns>Se a função tiver êxito, o valor de retorno é diferente de zero.</returns>
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool WriteProcessMemory(int hProcess, int lpBaseAddress,
+            byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesWritten);
     }
 }
